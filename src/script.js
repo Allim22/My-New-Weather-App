89a6d77a289a805925598e47dbbc5a01b40453c9
Allim22 <148294652+Allim22@users.noreply.github.com>
2023-11-02 19:05:47 -0400

    added functionality to change weather numerical value

--- src/script.js
+++ src/script.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temp-value");
+  currentTemperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(currentTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "c22cfddt3bf5bdc2752045c984obafed";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form-id");
